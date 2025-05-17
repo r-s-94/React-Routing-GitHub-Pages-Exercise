b6cb9636f32564fe5606b2091a72a3e7652a00fe
r.s. <richter.s.94@web.de>
2025-05-17 18:28:24 +0200

    Add gh action

--- src/exampleComponent2.tsx
+++ src/exampleComponent2.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function ExampleComponent2() {
   return (
     <>
-      <h1>Example-Test5-Komponente 2</h1>
+      <h1>Example-Test-Komponente 2</h1>
       <Link to="/">zurück</Link>
     </>
   );
